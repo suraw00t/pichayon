10a44a2ed0e678a46445197bd420f85bbd9f7d17
somboon <mypspgood@gmail.com>
2020-02-10 21:28:04 +0700

    data storage manager was edited

--- pichayon/controller_node/data_storage.py
+++ pichayon/controller_node/data_storage.py
@@ -11,6 +11,17 @@ class DataStorage:
         self.query = Query()
 
     def initial_data_after_restart(self, data):
+        self.db.purge()
+        self.db.insert({'passcode': data['passcode'], 'type': 'passcode'})
+        user_groups = data['user_groups']
+        for group in user_groups:
+            for member in group['members']:
+                user = self.db.search(self.query.username==member['username'])
+                if user:
+                    continue
+                self.db.insert({'username': member['username'], 'rfid': member['rfid'], 'type':'user'})
+
+    def update_data(self, data):
         logger.debug(f'>>>>>>>{data}')
         passcode = self.db.search(self.query.type=='passcode')
         if passcode:
@@ -18,6 +29,9 @@ class DataStorage:
         else:
             self.db.insert({'passcode': data['passcode'], 'type': 'passcode'})
         user_groups = data['user_groups']
+        users = self.db.search(self.query.type=='user')
+        for user in users:
+            
         for group in user_groups:
             for member in group['members']:
                 user = self.db.search(self.query.username==member['username'])
