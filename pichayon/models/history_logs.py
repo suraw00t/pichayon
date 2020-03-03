30db4f8eb3f14710a7e99c1c67d98522da09c5b3
somboon <mypspgood@gmail.com>
2020-03-03 23:42:58 +0700

    models logs was edited

--- pichayon/models/history_logs.py
+++ pichayon/models/history_logs.py
@@ -6,8 +6,8 @@ class HistoryLog(me.Document):
     actor = me.ReferenceField('User', dbref=True)
     user = me.ReferenceField('User', dbref=True)
     action = me.StringField()
-    door_group = me.StringField()
-    user_group = me.StringField()
+    message = me.StringField()
+    details = me.DictField()
     recorded_date = me.DateTimeField(required=True,
                                      default=datetime.datetime.now)
 
