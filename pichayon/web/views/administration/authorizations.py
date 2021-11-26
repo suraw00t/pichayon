f68d2535f82b704c715f854a26868e18729d2543
Thanathip Limna <boatkrap@gmail.com>
2021-11-26 21:44:28 +0700

    fix layout

--- pichayon/web/views/administration/authorizations.py
+++ pichayon/web/views/administration/authorizations.py
@@ -36,9 +36,11 @@ def add_or_edit(auth_id):
     group_auth = None
     if auth_id:
         group_auth = models.GroupAuthorization.objects.get(id=auth_id)
+        form = AuthorityForm(obj=group_auth)
+        form.door_group.data = str(group_auth.door_group.id)
+        form.user_group.data = str(group_auth.user_group.id)
 
     if group_auth and request.method == 'GET':
-        form = AuthorityForm(obj=group_auth)
         form.start_time.data = datetime.time(
                 group_auth.rrule.start_time[0], 
                 group_auth.rrule.start_time[1],
@@ -51,20 +53,24 @@ def add_or_edit(auth_id):
                 )
         form.days.data = group_auth.rrule.days
 
-    form.door_group.choices = [(str(d.id), d.name) for d in door_groups]
-    form.user_group.choices = [(str(u.id), u.name) for u in user_groups]
+
+
+    form.door_group.choices = sorted(
+            [(str(d.id), d.name) for d in door_groups],
+            key=lambda x: x[1])
+    form.user_group.choices = sorted(
+            [(str(u.id), u.name) for u in user_groups],
+            key=lambda x: x[1])
 
     if not form.validate_on_submit():
-        return render_template('/administration/authorizations/add_authority.html',
+        return render_template('/administration/authorizations/add_edit.html',
                                form=form,
                                )
     # print(form.end_time.data)
     # print(form.start_time.data)
     if not group_auth:
-        print('hear')
         group_auth = models.GroupAuthorization()
 
-    print('---->', group_auth)
     rrule = models.Rrule()
     days = []
     for d in form.days.data:
@@ -91,46 +97,6 @@ def add_or_edit(auth_id):
     return redirect(
             url_for('administration.authorizations.index')
             )
-
-
-# @acl.allows.requires(Or(acl.is_admin, acl.is_supervisor))
-@acl.role_required('admin')
-def edit(auth_id):
-    group_auth = models.GroupAuthorization.objects.get(id=auth_id)
-
-    form = AuthorityForm(obj=group_auth)
-    selected_groupmember = None
-    rrule = None
-    for group_member in door_auth.authorization_groups:
-        if group_member.user_group == user_group:
-            selected_groupmember = group_member
-            rrule = group_member.rrule
-    choices = [(str(user_group.id), user_group.name)]
-    form.user_group.choices = choices
-    if not form.validate_on_submit():
-        h_start, m_start, _ = rrule.start_time.split(':')
-        h_end, m_end, _ = rrule.end_time.split(':')
-        form.days.data = [str(d) for d in rrule.days]
-        form.start_time.data = datetime.time(int(h_start), int(m_start))
-        form.end_time.data = datetime.time(int(h_end), int(m_end))
-        form.user_group.data = choices[0]
-        form.started_date.data = selected_groupmember.started_date
-        form.expired_date.data = selected_groupmember.expired_date
-        return render_template('/administration/authorizations/edit_authority.html',
-                               form=form,
-                               door_group=door_group,
-                               door_auth=door_auth)
-
-    for group_member in door_auth.authorization_groups:
-        if group_member.user_group == user_group:
-            group_member.started_date = form.started_date.data
-            group_member.expired_date = form.expired_date.data
-            group_member.rrule.days = [int(d) for d in form.days.data]
-            group_member.rrule.start_time = str(form.start_time.data)
-            group_member.rrule.end_time = str(form.end_time.data)
-    door_auth.save()
-    return redirect(url_for('administration.door_authorizations.index',
-                            group_id=doorgroup_id))
 
 
 @module.route('/<auth_id>/delete', methods=["GET", "POST"])
