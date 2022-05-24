7d868bc429dd3b993e761741b27e65c261de61ed
bomzyx <bomkung123456@hotmail.com>
2022-05-24 16:50:48 +0700

    Add request form model

--- pichayon/models/__init__.py
+++ pichayon/models/__init__.py
@@ -5,21 +5,23 @@ from .oauth2 import OAuth2Token
 from .groups import UserGroup, UserGroupMember, DoorGroup
 from .authorizations import GroupAuthorization, Rrule
 from .history_logs import HistoryLog
+from .request_form import RequestForm
 
-__all__ = [User,
-           Identity,
-           Door,
-           OAuth2Token,
-           UserGroup,
-           UserGroupMember,
-           DoorGroup,
-           GroupAuthorization,
-           # DoorGroupAuthorization,
-           # UserGroupAuthorization,
-           Rrule,
-           HistoryLog,
-           SparkbitDoorSystem
-           ]
+__all__ = [
+    User,
+    Identity,
+    Door,
+    OAuth2Token,
+    UserGroup,
+    UserGroupMember,
+    DoorGroup,
+    GroupAuthorization,
+    # DoorGroupAuthorization,
+    # UserGroupAuthorization,
+    Rrule,
+    HistoryLog,
+    SparkbitDoorSystem,
+]
 
 from flask_mongoengine import MongoEngine
 
@@ -32,15 +34,11 @@ def init_db(app):
 
 def init_mongoengine(settings):
     import mongoengine as me
-    dbname = settings.get('MONGODB_DB')
-    host = settings.get('MONGODB_HOST', 'localhost')
-    port = int(settings.get('MONGODB_PORT', '27017'))
-    username = settings.get('MONGODB_USERNAME', '')
-    password = settings.get('MONGODB_PASSWORD', '')
 
-    me.connect(db=dbname,
-               host=host,
-               port=port,
-               username=username,
-               password=password)
+    dbname = settings.get("MONGODB_DB")
+    host = settings.get("MONGODB_HOST", "localhost")
+    port = int(settings.get("MONGODB_PORT", "27017"))
+    username = settings.get("MONGODB_USERNAME", "")
+    password = settings.get("MONGODB_PASSWORD", "")
 
+    me.connect(db=dbname, host=host, port=port, username=username, password=password)
