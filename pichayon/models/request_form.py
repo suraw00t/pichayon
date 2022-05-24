96e3ff247bb4493c573560696904aa8f49c1c658
bomzyx <bomkung123456@hotmail.com>
2022-05-24 16:51:50 +0700

    remove semester too and request date

--- pichayon/models/request_form.py
+++ pichayon/models/request_form.py
@@ -6,9 +6,8 @@ from . import users
 
 
 class RequestForm(me.Document):
-    users = me.ReferenceField(user.User)
-    semester = me.IntField(required=True)
+    users = me.ReferenceField(users.User)
+    started_date = me.DateTimeField(required=True)
+    ended_date = me.DateTimeField(required=True)
     room = me.StringField()
     purpose = me.StringField(required=True)
-    tool = me.StringField()
-    request_date = me.DateTimeField(required=True, default=datetime.datetime.now)
