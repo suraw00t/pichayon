25d3d2e41627afae7622760148c593c198c5238c
Napat Chalerwong <bomzyx123456@gmail.com>
2022-06-05 14:28:24 +0700

    Update two forms

--- pichayon/web/forms/request_forms.py
+++ pichayon/web/forms/request_forms.py
@@ -1,15 +1,34 @@
-from wtforms import Form
-from wtforms import fields
-from wtforms import validators
+from wtforms import Form, fields, validators
+from pichayon import models
 
 import datetime
 
 from flask_wtf import FlaskForm
+from flask_mongoengine.wtf import model_form
+
+RequestForm = model_form(
+    models.RoomRequest,
+    FlaskForm,
+    exclude=[
+        "users"
+    ],
+
+    field_args = {
+        "users": {"label": "Users"},
+        "started_date": {"label": "Started"},
+        "ended_date": {"label": "Ended"},
+        "room": {"label": "Room"},
+        "purpose": {"label": "Purpose"},
+    },
+)
+
+class RequestForm(RequestForm):
+    pass
 
 
-class RequestForm(FlaskForm):
-    started_date = fields.DateTimeField()
-    ended_date = fields.DateTimeField()
-    room = fields.StringField("Room")
-    purpose = fields.StringField("Purpose")
+# class RequestForm(FlaskForm):
+#     started_date = fields.DateTimeField()
+#     ended_date = fields.DateTimeField()
+#     room = fields.StringField("Room", validators=[validators.InputRequired()])
+#     purpose = fields.StringField("Purpose", validators=[validators.InputRequired()])
     
