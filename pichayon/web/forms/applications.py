78a5460e745048a568cce43b13d6e22fc891c70e
Napat Chalerwong <bomzyx123456@gmail.com>
2022-07-24 13:00:02 +0700

    Moved remark from exclude to field_args

--- pichayon/web/forms/applications.py
+++ pichayon/web/forms/applications.py
@@ -9,13 +9,14 @@ from flask_mongoengine.wtf import model_form
 BaseApplicationForm = model_form(
     models.Application,
     FlaskForm,
-    exclude=["users","status","remark"],
+    exclude=["users","status"],
     field_args={
         "advisor": {"label": "Advisor"},
         "started_date": {"label": "Start Date", "format": "%Y-%m-%d %H:%M"},
         "ended_date": {"label": "End Date", "format": "%Y-%m-%d %H:%M"},
         "room": {"label": "Room", "label_modifier": lambda r: r.name},
         "purpose": {"label": "Purpose"},
+        "remark": {"label": "Remark"},
     },
 )
 
