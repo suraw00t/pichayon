935c0caa033b2c3993ec12a68604f5012daf2627
Napat Chalermwong <bomzyx123456@gmail.com>
2022-08-05 15:40:51 +0700

    Add create date, status, ip in models

--- pichayon/models/applications.py
+++ pichayon/models/applications.py
@@ -12,10 +12,12 @@ class Application(me.Document):
 
     advisor = me.StringField(required=True)
 
+    created_date = me.DateTimeField(default=datetime.datetime.now)
     started_date = me.DateTimeField(required=True, default=datetime.datetime.now())
     ended_date = me.DateTimeField(required=True)
 
     room = me.ReferenceField("Door", dbref=True)
     purpose = me.StringField(required=True)
-    status = me.StringField(required=True, default="Pending")
+    status = me.StringField(required=True, default="pending")
     remark = me.StringField()
+    ip_address = me.StringField(max_length=255)
