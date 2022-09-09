a6e46e24d343f3a872a45777a41e38a587fd6bac
bomzyx <bomkung123456@hotmail.com>
2022-09-09 16:04:40 +0700

    Update the advisor

--- pichayon/web/forms/applications.py
+++ pichayon/web/forms/applications.py
@@ -14,7 +14,7 @@ BaseApplicationForm = model_form(
         "degree": {"label": "Degrees"},
         "advisor": {
             "label": "Advisor",
-            "label_modifier": lambda ad: ad.first_name,
+            "label_modifier": lambda ad: f"{ad.first_name} {ad.last_name}",
         },
         "started_date": {"label": "Start Date", "format": "%Y-%m-%d %H:%M"},
         "ended_date": {"label": "End Date", "format": "%Y-%m-%d %H:%M"},
