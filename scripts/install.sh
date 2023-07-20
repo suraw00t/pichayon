4a74bf509b664bbfc873b213c4bcabf21f3bd4b0
Thanathip Limna <boatkrap@mail.com>
2023-07-20 21:10:56 +0700

    fix python env

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,43 @@
-sudo apt install rustc libssl-dev libffi-dev
+#!/usr/bin/bash
+
+
+if [ ! -d venv ]
+then
+    echo "create python virtual env"
+    python3 -m venv venv
+else
+    echo "python virtual env found"
+fi
+
+export PYTHON=$(pwd)/venv/bin/python
+
+echo "install poetry"
+$PYTHON -m pip install poetry
+
+echo "install door controller"
+cd door-controller
+$PYTHON -m poetry install
+cd ..
+
+
+echo "install package"
+sudo apt install -y rustc libssl-dev libffi-dev
+
+echo "copy service file"
 sudo cp scripts/pichayon-door.service /lib/systemd/system
-sudo mkdir /var/log/pichayon
-sudo chown -R $USER: /var/log/pichayon
+
+if [ ! -d /var/log/pichayon ]
+then
+    echo "create log directory"
+    sudo mkdir /var/log/pichayon
+    sudo chown -R $USER: /var/log/pichayon
+else
+    echo "pichayon log found"
+fi
+
+
+
+echo "enable service"
 sudo systemctl daemon-reload
 sudo systemctl enable pichayon-door.service
 
