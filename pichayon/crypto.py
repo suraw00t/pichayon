6f868ac2d430fb2e6aab87b0a0a10aef617d683d
suraw00t <surawut.kong@gmail.com>
2023-09-19 15:24:42 +0700

    fix: crypto

--- pichayon/crypto.py
+++ pichayon/crypto.py
@@ -2,6 +2,7 @@ import base64
 import hashlib
 from Crypto import Random
 from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
 
 
 class AESCrypto:
@@ -18,14 +19,21 @@ class AESCrypto:
             padding = AES.block_size - msg_mod
             message = message + (" " * padding)
 
-        return base64.b64encode(iv + cipher.encrypt(message)).decode("utf-8")
+        padded_message = pad(message.encode("utf-8"), AES.block_size)
+        ciphertext_bytes = cipher.encrypt(padded_message)
+        ciphertext = base64.b64encode(iv + ciphertext_bytes).decode("utf-8")
+        
+        return ciphertext
 
     def decrypt(self, encode_text):
         cipher_text = base64.b64decode(encode_text)
         iv = cipher_text[:16]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        decrypted_bytes =  cipher.decrypt(cipher_text[16:])
+        plaintext_bytes = unpad(decrypted_bytes, AES.block_size)
+        plaintext =  plaintext_bytes.decode('utf-8').strip()
 
-        return cipher.decrypt(cipher_text[16:]).decode("utf8").strip()
+        return plaintext
 
 
 def init_crypto(app):
