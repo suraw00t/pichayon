86a21ca8b13cc5ec56c9cf8727d5d8f376e1568c
suraw00t <surawut.kong@gmail.com>
2023-09-25 21:34:36 +0700

    increase beep timing

--- pichayon/door_controller/server.py
+++ pichayon/door_controller/server.py
@@ -243,7 +243,7 @@ class DoorControllerServer:
             ):
                 self.device.is_force_unlock = False
                 await self.device.lock_door()
-                await self.device.play_success_access_sound(0.5)
+                await self.device.play_success_access_sound(1)
                 logger.debug("Force unlock is turned off")
 
             await asyncio.sleep(0.1)
