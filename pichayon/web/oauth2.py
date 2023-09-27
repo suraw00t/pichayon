efead86e050aa32765dae903e1fc4ac5bb32a905
suraw00t <surawut.kong@gmail.com>
2023-09-27 11:56:52 +0700

    fix update user info

--- pichayon/web/oauth2.py
+++ pichayon/web/oauth2.py
@@ -94,7 +94,7 @@ def create_user_psu(user_info, user=None):
             system_id=user_info.get("psu_id", user_info.get("username")),
             status="active",
         )
-    elif "psu" not in user.resources:
+    else:
         user.first_name = user_info.get("first_name", "").title()
         user.last_name = user_info.get("last_name", "").title()
         user.first_name_th = user_info.get("first_name_th", "")
@@ -234,11 +234,7 @@ def handle_authorized_oauth2(remote, token):
 
     user = None
     if remote.name == "psu":
-        user = models.User.objects(
-            username=user_info.get("username"),
-            first_name_th__ne="",
-            last_name_th__ne="",
-        ).first()
+        user = models.User.objects(username=user_info.get("username")).first()
     elif "email" in user_info and user_info["email"]:
         user = models.User.objects(me.Q(email=user_info.get("email"))).first()
     elif "sub" in user_info:
