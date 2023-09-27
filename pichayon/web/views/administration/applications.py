7b57009d72f9fc37264d8b105a6a117770a96aba
suraw00t <surawut.kong@gmail.com>
2023-09-27 16:43:50 +0700

    add pagination

--- pichayon/web/views/administration/applications.py
+++ pichayon/web/views/administration/applications.py
@@ -12,6 +12,7 @@ from flask import (
 
 from flask_login import login_user, logout_user, login_required, current_user
 from pichayon.web.client.pichayon_client import pichayon_client
+from flask_mongoengine import Pagination
 
 
 from pichayon import models
@@ -24,7 +25,8 @@ module = Blueprint("applications", __name__, url_prefix="/applications")
 @module.route("")
 @acl.role_required("admin", "lecturer")
 def index():
-    applications = models.Application.objects(status__ne="approved").order_by("-id")
+    applications = models.Application.objects(status="pending").order_by("created_date")
+    page = request.args.get("page", default=1, type=int)
 
     if "admin" not in current_user.roles:
         user = current_user._get_current_object()
@@ -34,15 +36,21 @@ def index():
 
             return redirect(url_for("applications.approve"))
 
+    paginated_applications = Pagination(applications, page=page, per_page=30)
+
     return render_template(
-        "/administration/applications/index.html", applications=applications
+        "/administration/applications/index.html",
+        paginated_applications=paginated_applications,
     )
 
 
 @module.route("/approved")
 @acl.role_required("admin", "lecturer")
 def approved():
-    applications = models.Application.objects(status="approved").order_by("-id")
+    applications = models.Application.objects(status__ne="pending").order_by(
+        "-approved_date"
+    )
+    page = request.args.get("page", default=1, type=int)
 
     if "admin" not in current_user.roles:
         user = current_user._get_current_object()
@@ -52,8 +60,11 @@ def approved():
 
             return redirect(url_for("applications.approve"))
 
+    paginated_applications = Pagination(applications, page=page, per_page=30)
+
     return render_template(
-        "/administration/applications/index.html", applications=applications
+        "/administration/applications/index.html",
+        paginated_applications=paginated_applications,
     )
 
 
@@ -90,16 +101,21 @@ def add_or_edit_user_to_user_group(application_id):
     if "admin" in current_user.roles:
         user_groups = models.UserGroup.objects(status="active").order_by("name")
     else:
-        user_group_members = models.UserGroupMember.objects(
+        admin_group_members = models.UserGroupMember.objects(
             role="admin", user=current_user._get_current_object()
         ).order_by("name")
 
         user_groups = [
-            user_group_member.group for user_group_member in user_group_members
+            user_group_member.group for user_group_member in admin_group_members
         ]
 
+    user_group_members = models.UserGroupMember.objects(user=application.user)
+    groups_of_user = [ugm.group for ugm in user_group_members]
+
     form.user_groups.choices = [
-        (str(user_group.id), user_group.name) for user_group in user_groups
+        (str(user_group.id), user_group.name)
+        for user_group in user_groups
+        if user_group not in groups_of_user
     ]
 
     if request.method == "GET":
@@ -130,8 +146,8 @@ def add_or_edit_user_to_user_group(application_id):
                 started_date=form.started_date.data,
                 expired_date=form.expired_date.data,
                 added_by=current_user._get_current_object(),
-                # application=application,
             )
+
         user_group_member.application = application
         user_group_member.started_date = form.started_date.data
         user_group_member.expired_date = form.expired_date.data
