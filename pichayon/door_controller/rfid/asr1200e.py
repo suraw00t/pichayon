f848500beaa78252974ccb88526f032950696376
Thanathip Limna <boatkrap@gmail.com>
2024-09-23 17:27:03 +0700

    reverse name

--- pichayon/door_controller/rfid/asr1200e.py
+++ pichayon/door_controller/rfid/asr1200e.py
@@ -107,13 +107,11 @@ class WiegandReader(readers.Reader):
 
         if data[1:17].count(1) % 2 == 0:
             # new version
-            # if not even_parity_bit:
-            if even_parity_bit:
+            if not even_parity_bit:
                 return False
 
         if data[17:-1].count(1) % 2 == 1:
-            # if not odd_parity_bit:
-            if odd_parity_bit:
+            if not odd_parity_bit:
                 return False
 
         return True
@@ -123,11 +121,9 @@ class WiegandReader(readers.Reader):
 
         out = 0
         for bit in data:
-            # for new rpi version
-            # nbit = bit ^ 1
-            # out = (out << 1) | nbit
+            nbit = bit ^ 1
+            out = (out << 1) | nbit
 
-            out = (out << 1) | bit
         return f"{out:08X}"
 
     async def get_tag(self):
